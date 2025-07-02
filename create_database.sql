0cf54a00da6a93aaa44c05b28322b341bbaf6410
DieAllPies <dennisfamiliapedersen@gmail.com>
2025-07-02 15:18:59 -0300

    installed dotenv and updated migrate script

--- create_database.sql
+++ create_database.sql
@@ -1,31 +1,31 @@
-CREATE TABLE produtos (
+CREATE TABLE IF NOT EXISTS produtos (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(100) NOT NULL,
   preco DECIMAL(10, 2) NOT NULL,
   descricao TEXT
 );
 
-CREATE TABLE clientes (
+CREATE TABLE IF NOT EXISTS clientes (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(100) NOT NULL,
   email VARCHAR(100) UNIQUE NOT NULL
 );
 
-CREATE TABLE estoque (
+CREATE TABLE IF NOT EXISTS estoque (
   id INT AUTO_INCREMENT PRIMARY KEY,
   produto_id INT,
   quantidade INT NOT NULL,
   FOREIGN KEY (produto_id) REFERENCES produtos(id)
 );
 
-CREATE TABLE vendas (
+CREATE TABLE IF NOT EXISTS vendas (
   id INT AUTO_INCREMENT PRIMARY KEY,
   cliente_id INT,
   data_venda DATETIME DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (cliente_id) REFERENCES clientes(id)
 );
 
-CREATE TABLE venda_produto (
+CREATE TABLE IF NOT EXISTS venda_produto (
   id INT AUTO_INCREMENT PRIMARY KEY,
   venda_id INT,
   produto_id INT,
